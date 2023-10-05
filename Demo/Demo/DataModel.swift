f1fae46591d86508ae21973f82095820baf4941f
maksim_mitrofanov <keyseymiroff@gmail.com>
2023-10-05 16:50:56 +0400

    Made dataModel codable

--- Demo/Demo/DataModel.swift
+++ Demo/Demo/DataModel.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-struct DataModel: Identifiable {
+struct DataModel: Identifiable, Codable {
     let name: String
     let price: Int
     let keywords: [String]
-    let id: String = UUID().uuidString
+    let id: String
 }
 
 extension DataModel {
-    static let template1 = DataModel(name: "First Item", price: 100, keywords: ["red", "versatile", "inexpensive"])
-    static let template2 = DataModel(name: "Second Item", price: 150, keywords: ["yellow", "interesting", "available"])
-    static let template3 = DataModel(name: "Third Item", price: 190, keywords: ["green", "doorway", "out-of-stock"])
+    static let template1 = DataModel(name: "First Item", price: 100, keywords: ["red", "versatile", "inexpensive"], id: "123")
+    static let template2 = DataModel(name: "Second Item", price: 150, keywords: ["yellow", "interesting", "available"], id: "456")
+    static let template3 = DataModel(name: "Third Item", price: 190, keywords: ["green", "doorway", "out-of-stock"], id: "789")
 }
